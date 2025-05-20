959c1454448f7dd858f7037e22128e6524e1fb8c
alvarorp1907 <alvaro.rodriguez.pineiro@alumnos.upm.es>
2025-05-20 20:53:37 +0200

    status code emitted by endorsement peers is checked when chaincode is invoked in hyperledger Fabric

--- IpfsToBlockchain.py
+++ IpfsToBlockchain.py
@@ -1,22 +1,30 @@
 import requests
 import subprocess
 import os
+import time
 
 #constants
-LOCAL_DIR_FILES = "/home/alvarorp19/archivesTestingIPFS"
-LOCAL_DIR_HYPERLEDGER_TEST_NET = r"/home/alvarorp19/fabric-samples/test-network"
+DIR_FILES = "/home/alvarorp19/archivesTestingIPFS"
+DIR_HYPERLEDGER_NET = "/home/alvarorp19/fabric-samples/test-network"
+
+DIR_CERT_CA_ORG1 = f"{DIR_HYPERLEDGER_NET}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt"
+DIR_CERT_CA_ORG2 = f"{DIR_HYPERLEDGER_NET}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt"
+DIR_CERT_CA_ORDERER =f"{DIR_HYPERLEDGER_NET}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+
+CHAINCODE_NAME = "chaincode"
+CHAINCODE_CHANNEL = "mychannel"
+
+INVOKE_CN_BASE_CMD = r"peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com " \
+                     f"--tls --cafile {DIR_CERT_CA_ORDERER} -C {CHAINCODE_CHANNEL} -n {CHAINCODE_NAME} " \
+                     f"--peerAddresses localhost:7051 --tlsRootCertFiles {DIR_CERT_CA_ORG1} " \
+                     f"--peerAddresses localhost:9051 --tlsRootCertFiles {DIR_CERT_CA_ORG2} -c"
+                     
+QUERY_CN_BASE_CMD = f"peer chaincode query -C {CHAINCODE_CHANNEL} -n {CHAINCODE_NAME} -c"
 
 #variables
 #IPFS
-fileIPFSCID = None
-fileIPFS = f"{LOCAL_DIR_FILES}/prueba.txt"
-
-#Hyperledger Fabric
-HyperledgerCmd = {"InitLedger" : r"peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile " \
-                  r"/home/alvarorp19/fabric-samples/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n chaincode " \
-                  r"--peerAddresses localhost:7051 --tlsRootCertFiles /home/alvarorp19/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt " \
-                  r"--peerAddresses localhost:9051 --tlsRootCertFiles /home/alvarorp19/fabric-samples/test-network/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt " \
-                  "-c \'{\"Args\":[\"InitLedger\"]}\'"}
+fileIPFSCID = "0"
+fileIPFS = f"{DIR_FILES}/prueba5.txt"
 
 #Stage 1 -> upload file to IPFS through local API server
 
@@ -38,13 +46,23 @@ else:
 
 #stage 2 -> invoke the chaincode installed in the hyperledger Fabric network to store the file's CID
 
-os.chdir(LOCAL_DIR_HYPERLEDGER_TEST_NET)
+#Hyperledger Fabric commands
+HyperledgerCmd = {"InitLedger"   : INVOKE_CN_BASE_CMD + " \'{\"Args\":[\"InitLedger\"]}\'",
+                  "AddNewAsset"  : INVOKE_CN_BASE_CMD + " \'{\"Args\":[\"AddNewAsset\"," + f"\"{fileIPFS}\",\"{fileIPFSCID}\"]" + "}\'",
+                  "GetAllAssets" : QUERY_CN_BASE_CMD + " \'{\"Args\":[\"GetAllAssets\"]}\'"}
+
+os.chdir(DIR_HYPERLEDGER_NET)
 
 #ToDo : set environment variables before invoking the chaincode
-res = subprocess.run(HyperledgerCmd["InitLedger"], capture_output=True, text=True, shell=True)
-
-print(res)
-#ToDo: check status code from chaincode
+res = subprocess.run(HyperledgerCmd["AddNewAsset"], capture_output=True, text=True, shell=True)
+cleanRes = res.stderr.strip(' \n\r')
+transactionStatusCode = cleanRes[-3:]
+#time.sleep(2)
+#res2 = subprocess.run(HyperledgerCmd["GetAllAssets"], capture_output=True, text=True, shell=True)
+if (transactionStatusCode == "200"):
+    print(f"CID successfully stored in Hyperledger Fabric Blockchain")
+else:
+    print(f"Error invoking the chaincode in Hyperledger Fabric Blockchain: {res}")
 
 
 
