8aa050dcbc6674f12fdb439dcba9b9051192245b
alvarorp1907 <alvaro.rodriguez.pineiro@alumnos.upm.es>
2025-05-23 19:53:55 +0200

    a deamon has been implemented to reach a higher level of automation suring the whole process

--- IpfsToBlockchain.py
+++ IpfsToBlockchain.py
@@ -1,10 +1,12 @@
 import requests
 import subprocess
 import os
+import sys
 import time
 from multiprocessing import Process
 from pathlib import Path
 import json
+from daemonClass import daemon
 
 #################### constants ####################
 
@@ -44,152 +46,207 @@ HyperledgerEnvVar = {"FABRIC_CFG_PATH" : f"{DIR_HYPERLEDGER_FABRIC_SAMPLES}/conf
                      "CORE_PEER_ADDRESS": "localhost:7051"}
 
 
-#################### functions ####################
+#################### MonitoringForIpfsHyperledger class ####################
 
 
-def uploadFileToIPFS(file):
-    """
-    Def:
-        Function to upload a file to IPFS.
-    Args:
-        targetFile : full path of the file.
-    Return:
-        Tuple containing the request status and IPFS CID obtained.
-    Note:
-        None
-    """
-    retStatus = True
-    fileIPFSCID = CID_CODE_ERROR
-    fileIPFSJson = {'fileIPFS': open(file, 'rb')}
-    
-    response = requests.post('http://127.0.0.1:5001/api/v0/add', files=fileIPFSJson)
+class MonitoringForIpfsHyperledger(daemon):
 
-    #checking respose status from local API server
-    if response.status_code == 200:
-        payload = response.json()
-        fileIPFSCID = payload['Hash']
-        print(f"File named as {file} successfully uploaded to IPFS.")
-        print(f"{file} CID: {fileIPFSCID}")
-        print(f"Public URL: https://ipfs.io/ipfs/{fileIPFSCID}")
-    else:
-        print(f"File name as {file} cannot be uploaded to IPFS")
-        print(f"Error code {response.status_code} received from local API server")
-        retStatus = False
-      
-    return retStatus,fileIPFSCID
-
-
-
-def filesMonitoring():
-    """
-    Def:
-        Function to monitor if a new file has been added in the directory where target
-        files must be stored
-    Args:
-        Void 
-    Return:
-        Void   
-    Note:
-        This function is executed in a background process
-    """
-    isFirstIteration = True
-    lastFileModified = ""#path of the last file modified
-    lastTimeStamp = 0 #timeStamp of the last file modified
-    targetDir = Path(DIR_FILES)
-    
-    #set environment variables before any operation in Hyperledger
-    for environVarName, environVarValue in HyperledgerEnvVar.items():
-        os.environ[environVarName] = environVarValue
-    
-    while True:
-        #analyzing target file where IPFS file are being stored
-        #print(f"Last modification in IPFS dir -> {lastTimeStamp}")
+    def __init__(self,pidFile,debugLevel):
+        """
+        Class constructor
+        """
+        super().__init__(pidFile,debugLevel)
         
-        for file in targetDir.iterdir():
-            if file.is_file():
-                currTimestamp = file.stat().st_mtime
-                #print(f"{file.name} → última modificación: {currTimestamp}")
-                if lastTimeStamp < currTimestamp:
-                    #updating last modified file info
-                    lastTimeStamp = currTimestamp
-                    lastFileModified = file.name
-                    fullPathFile = f"{DIR_FILES}/{lastFileModified}"
-                    #if is the first iteration we dont do any operation
-                    #in IPFS and Hyperledger
-                    if isFirstIteration == False:
-                        print(f"File {lastFileModified} has been modified in target directory : {DIR_FILES}")
-                        #uploading last modifications to IPFS
-                        status,cid = uploadFileToIPFS(fullPathFile)
-                        
-                        if status:
-                            #checking if we need to summit modifications to IPFS and blockchain
-                            isCidStored = isIPFfileStoredInHyperledger(fullPathFile,cid)
-                            if isCidStored == False:
-                                #upload IPFS file to Hyperledger Fabric
-                                print(f"Uploading info from file '{fullPathFile}' to Hyperledger Fabric!")
-                                uploadFileToHyperledgerFabric(fullPathFile,cid)
-                            else:
-                                print(f"CID '{cid} has been previously stored in Hyperledger'")
-                        
-        isFirstIteration = False           
-        time.sleep(5)
-
-
-
-def isIPFfileStoredInHyperledger(targetFile,cidToCheck):
-    """
-    Def:
-        Function to check if the file specified at first parameter
-        is stored in Hyperledger Fabric or not.
-    Args:
-        targetFile : full path of the target file
-    Return:
-        True if found otherwise False   
-    Note:
-        None
-    """
-    ret = False
+        
     
-    res = subprocess.run(HyperledgerCmd["GetInfoAsset"].format(targetFile), capture_output=True, text=True, shell=True)
-    stdout = res.stdout
+    def uploadFileToIPFS(self,file):
+        """
+        Def:
+            Function to upload a file to IPFS.
+        Args:
+            targetFile : full path of the file.
+        Return:
+            Tuple containing the request status and IPFS CID obtained.
+        Note:
+            None
+        """
+        retStatus = True
+        fileIPFSCID = CID_CODE_ERROR
+        fileIPFSJson = {'fileIPFS': open(file, 'rb')}
+        
+        response = requests.post('http://127.0.0.1:5001/api/v0/add', files=fileIPFSJson)
     
-    if stdout != "":
-        dictInfoFile = json.loads(stdout)
-        print(f"obtained dict from Hyperledger -> {dictInfoFile}")
-        CidReadHyperledger = dictInfoFile["cid"]
-        # print(dictInfoFile["cid"])
-        if cidToCheck == CidReadHyperledger:
-            print("Nothing to store")
-            ret = True
+        #checking respose status from local API server
+        if response.status_code == 200:
+            payload = response.json()
+            fileIPFSCID = payload['Hash']
+            print(f"File named as {file} successfully uploaded to IPFS.")
+            print(f"{file} CID: {fileIPFSCID}")
+            print(f"Public URL: https://ipfs.io/ipfs/{fileIPFSCID}")
+        else:
+            print(f"File name as {file} cannot be uploaded to IPFS")
+            print(f"Error code {response.status_code} received from local API server")
+            retStatus = False
+        
+        return retStatus,fileIPFSCID
+    
+    
+    
+    def isIPFfileStoredInHyperledger(self,targetFile,cidToCheck):
+        """
+        Def:
+            Function to check if the file specified at first parameter
+            is stored in Hyperledger Fabric or not.
+        Args:
+            targetFile : full path of the target file
+        Return:
+            True if found otherwise False   
+        Note:
+            None
+        """
+        ret = False
+        
+        res = subprocess.run(HyperledgerCmd["GetInfoAsset"].format(targetFile), capture_output=True, text=True, shell=True)
+        stdout = res.stdout
+        
+        if stdout != "":
+            dictInfoFile = json.loads(stdout)
+            print(f"obtained dict from Hyperledger -> {dictInfoFile}")
+            CidReadHyperledger = dictInfoFile["cid"]
+            if cidToCheck == CidReadHyperledger:
+                print("Nothing to store")
+                ret = True
+                
+        return ret
+    
+    
+    
+    def uploadFileToHyperledgerFabric(self,fileName,cid):
+        """
+        Def:
+            Function to upload a file to Hyperledger Fabric Blockchain.
+        Args:
+            targetFile : full path of the file.
+            cid : IPFS CID
+        Return:
+            True if operation is successfully done otherwise False.
+        Note:
+            None
+        """
+        res = subprocess.run(HyperledgerCmd["AddNewAsset"].format(fileName,cid), capture_output=True, text=True, shell=True)
+        cleanRes = res.stderr.strip(' \n\r')
+        transactionStatusCode = cleanRes[-3:]
+        
+        if (transactionStatusCode == "200"):
+            print(f"CID '{cid}' successfully stored in Hyperledger Fabric Blockchain")
+        else:
+            print(f"Error invoking the chaincode in Hyperledger Fabric Blockchain: {res}")
+    
+    
+    
+    def filesMonitoring(self):
+        """
+        Def:
+            Function to monitor if a new file has been added in the directory where target
+            files must be stored
+        Args:
+            Void 
+        Return:
+            Void   
+        Note:
+            This function is executed in a background process
+        """
+        isFirstIteration = True
+        lastFileModified = ""#path of the last file modified
+        lastTimeStamp = 0 #timeStamp of the last file modified
+        targetDir = Path(DIR_FILES)
+        
+        #set environment variables before executing any operation in Hyperledger
+        for environVarName, environVarValue in HyperledgerEnvVar.items():
+            os.environ[environVarName] = environVarValue
+        
+        while True:
+            #analyzing target file where IPFS file are being stored
+            #print(f"Last modification in IPFS dir -> {lastTimeStamp}")
             
-    return ret
-
-
-
-def uploadFileToHyperledgerFabric(fileName,cid):
-    """
-    Def:
-        Function to upload a file to Hyperledger Fabric Blockchain.
-    Args:
-        targetFile : full path of the file.
-        cid : IPFS CID
-    Return:
-        True if operation is successfully done otherwise False.
-    Note:
-        None
-    """
-    res = subprocess.run(HyperledgerCmd["AddNewAsset"].format(fileName,cid), capture_output=True, text=True, shell=True)
-    cleanRes = res.stderr.strip(' \n\r')
-    transactionStatusCode = cleanRes[-3:]
+            for file in targetDir.iterdir():
+                if file.is_file():
+                    currTimestamp = file.stat().st_mtime
+                    #print(f"{file.name} → última modificación: {currTimestamp}")
+                    if lastTimeStamp < currTimestamp:
+                        #updating last modified file info
+                        lastTimeStamp = currTimestamp
+                        lastFileModified = file.name
+                        fullPathFile = f"{DIR_FILES}/{lastFileModified}"
+                        #if is the first iteration we dont do any operation
+                        #in IPFS and Hyperledger
+                        if isFirstIteration == False:
+                            print(f"File {lastFileModified} has been modified in target directory : {DIR_FILES}")
+                            #uploading last modifications to IPFS
+                            status,cid = self.uploadFileToIPFS(fullPathFile)
+                            
+                            if status:
+                                #checking if we need to summit modifications to IPFS and blockchain
+                                isCidStored = self.isIPFfileStoredInHyperledger(fullPathFile,cid)
+                                if isCidStored == False:
+                                    #upload IPFS file to Hyperledger Fabric
+                                    print(f"Uploading info from file '{fullPathFile}' to Hyperledger Fabric!")
+                                    self.uploadFileToHyperledgerFabric(fullPathFile,cid)
+                                else:
+                                    print(f"CID '{cid} has been previously stored in Hyperledger'")
+                            
+            isFirstIteration = False           
+            time.sleep(5)
     
-    if (transactionStatusCode == "200"):
-        print(f"CID '{cid}' successfully stored in Hyperledger Fabric Blockchain")
-    else:
-        print(f"Error invoking the chaincode in Hyperledger Fabric Blockchain: {res}")
+    
+    
+    def run(self):
+        """
+        Def:
+            Deamon routine to be executed in a background process.
+        Args:
+            Void.
+        Return:
+            Void.   
+        Note:
+            None.
+        """
+        self.filesMonitoring()
 
-
-
+        
+     
 #################### main ####################
 
 if __name__ == "__main__":
-    filesMonitoring()
+    
+    if len(sys.argv) == 3:
+    
+        debugLevel = None
+        
+        #analyzing 3rd arg
+        match sys.argv[2]:
+            case "debugOn":
+                debugLevel = True
+            case "debugOff":
+                debugLevel = False
+            case _:
+                print("The 3rd program argument is unknown")
+                sys.exit(2)
+                
+        daemon = MonitoringForIpfsHyperledger('/home/alvarorp19/scriptsPyTFM/daemon_IPFS_Hyperledger.pid',debugLevel)
+        
+        #analyzing 2nd arg
+        match sys.argv[1]:
+            case "start":
+                daemon.start()
+            case "stop":
+                daemon.stop()
+            case "restart":
+                daemon.restart()
+            case _:
+                print("The 2nd program argument is unknown")
+                sys.exit(2)
+        sys.exit(0)
+    else:
+        print(f"usage: {sys.argv[0]} start|stop|restart debugOn|debugOff")
+        sys.exit(2) 
